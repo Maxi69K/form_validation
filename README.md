0d8f12a26e8d5312c7257a207e4fdc3419d1550b
Goran Kekez <gorankekez@gmail.com>
2022-11-04 23:10:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# voyage-tasks
+
+Your project's `readme` is as important to success as your code. For 
+this reason you should put as much care into its creation and maintenance
+as you would any other component of the application.
+
+If you are unsure of what should go into the `readme` let this article,
+written by an experienced Chingu, be your starting point - 
+[Keys to a well written README](https://tinyurl.com/yk3wubft).
+
+And before we go there's "one more thing"! Once you decide what to include
+in your `readme` feel free to replace the text we've provided here.
+
+> Own it & Make it your Own!
+
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+- Red: hsl(0, 100%, 74%) 
+- Green: hsl(154, 59%, 51%)
+
+### Accent
+
+- Blue: hsl(248, 32%, 49%)
+
+### Neutral
+
+- Dark Blue: hsl(249, 10%, 26%) 
+- Grayish Blue: hsl(246, 25%, 77%)
+
+## Typography
+
+### Body Copy
+
+- Font size: 16px
+
+### Font
+
+- Family: [Poppins](https://fonts.google.com/specimen/Poppins)
+- Weights: 400, 500, 600, 700
+
+
+28.10.22 created based template - Maxi69K
+
+29.10.22 Completed HTML and CSS - Maxi69K
+*The complete layout of the page is complete width full response.
+*The site is mobile first responsive.
+*Added essential constants in JavaScript.
---
+++ js/main.js
@@ -0,0 +1,150 @@
+// v41-toucans-team-03
+
+// Second box
+const secondBox = document.getElementById('second-box')
+// Form
+const myForm = document.getElementById('my-form')
+// First Name
+const firstName = document.getElementById('first-name')
+const errorFirstNameBox = document.getElementById('error-first-name')
+// Last Name
+const lastName = document.getElementById('last-name')
+const errorLastNameBox = document.getElementById('error-last-name')
+//E-mail
+const email = document.getElementById('e-mail')
+const errorEmailBox = document.getElementById('error-email')
+// Password
+const password = document.getElementById('user-password')
+const errorPasswordBox = document.getElementById('error-password')
+// Save button
+const saveBtn = document.getElementById('save-btn')
+// Terms and Services span
+const termsAndServices = document.querySelector('#terms-box span')
+// New user
+let newUser = { firstName: '', lastName: '', email: '', password: '' }
+
+// Save button function
+saveBtn.addEventListener('click', formValidation)
+
+function formValidation(e) {
+  e.preventDefault()
+
+  let userData = {
+    fnv: firstName.value,
+    lnv: lastName.value,
+    emv: email.value,
+    psv: password.value
+  }
+
+  let errorsName = []
+  let errorsLastName = []
+  let errorsEmail = []
+  let errorsPassword = []
+
+  // first name validation
+  if (userData.fnv === '') {
+    firstName.setAttribute('class', 'error')
+    errorFirstNameBox.style.display = 'block'
+    errorsName.push('First Name cannot be empty')
+  } else if (userData.fnv.length < 3 || userData.fnv.length > 15) {
+    firstName.setAttribute('class', 'error')
+    errorFirstNameBox.style.display = 'block'
+    errorsName.push('Min 3 max 15 characters')
+  }
+  // last name validation
+  if (userData.lnv === '') {
+    lastName.setAttribute('class', 'error')
+    errorLastNameBox.style.display = 'block'
+    errorsLastName.push('Last Name cannot be empty')
+  } else if (userData.lnv.length < 3 || userData.lnv.length > 15) {
+    lastName.setAttribute('class', 'error')
+    errorLastNameBox.style.display = 'block'
+    errorsLastName.push('Min 3 max 15 characters')
+  }
+  // email validation
+  if (userData.emv === '') {
+    email.setAttribute('class', 'error')
+    errorEmailBox.style.display = 'block'
+    errorsEmail.push('Email cannot be empty')
+  } else if (userData.emv.indexOf('@') === -1) {
+    email.setAttribute('class', 'error')
+    errorEmailBox.style.display = 'block'
+    errorsEmail.push('Looks like this is not email')
+  }
+  // password validation
+  if (userData.psv === '') {
+    password.setAttribute('class', 'error')
+    errorPasswordBox.style.display = 'block'
+    errorsPassword.push('Password cannot be empty')
+  } else if (userData.psv.length < 4 || userData.psv.length > 15) {
+    password.setAttribute('class', 'error')
+    errorPasswordBox.style.display = 'block'
+    errorsPassword.push('Min 4 max 15 characters')
+  }
+
+  if (errorsName.length !== 0 || errorsLastName.length !== 0 || errorsEmail.length !== 0 || errorsPassword.length !== 0) {
+    // there are errors
+    errorFirstNameBox.innerHTML = errorsName
+    errorLastNameBox.innerHTML = errorsLastName
+    errorEmailBox.innerHTML = errorsEmail
+    errorPasswordBox.innerHTML = errorsPassword
+
+  } else {
+    // everything's OK - submitting the form
+
+    // New user data
+    function newUserData() {
+      newUser.firstName = userData.fnv
+      newUser.lastName = userData.lnv
+      newUser.email = userData.emv
+      newUser.password = userData.psv
+      console.log(newUser)
+      saveNewUser()
+    }
+    newUserData()
+
+    /* setTimeout(function () {
+      //myForm.submit()
+
+      firstName.removeAttribute('class', 'error')
+      errorFirstNameBox.style.display = 'none'
+
+      lastName.removeAttribute('class', 'error')
+      errorLastNameBox.style.display = 'none'
+
+      email.removeAttribute('class', 'error')
+      errorEmailBox.style.display = 'none'
+
+      password.removeAttribute('class', 'error')
+      errorPasswordBox.style.display = 'none'
+    }, 2000) */
+    
+  }
+  
+}
+
+// Save new user to local storage
+function saveNewUser() {
+  let usersData = [
+    {
+      index: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    },
+  ]
+  usersData.index = Math.random(Math.floor(10000-10)+10)
+  usersData.firstName = newUser.firstName
+  usersData.lastName = newUser.lastName
+  usersData.email = newUser.email
+  usersData.password = newUser.password
+
+  console.log(usersData)
+
+  localStorage.userData = JSON.stringify(usersData)
+
+  //console.log(localStorage.accounts)
+  //console.log(JSON.parse(localStorage.accounts))
+}
+
